77c3f7106b88fd2eb0771efb9e4edbe40cece643
Lachezar Lechev <elpiel93@gmail.com>
2020-05-05 18:34:30 +0300

    bebop2 - add `Bebop2::landing()` & clean-up

--- bebop2/src/lib.rs
+++ bebop2/src/lib.rs
@@ -4,11 +4,11 @@ use arsdk_rs::{
     frame::{BufferID, Frame, Type},
 };
 
-pub use arsdk_rs::{ardrone3::ArDrone3, prelude::*};
+pub use arsdk_rs::{ardrone3::{Piloting, ArDrone3}, prelude::*};
 
 pub mod prelude {
     pub use crate::Bebop2;
-    pub use arsdk_rs::{ardrone3::ArDrone3, prelude::*};
+    pub use arsdk_rs::{ardrone3::{Piloting, ArDrone3}, prelude::*};
 }
 
 pub struct Bebop2 {
@@ -22,17 +22,22 @@ impl Bebop2 {
         Ok(Self { drone })
     }
 
-    // ARCOMMANDS_ID_ARDRONE3_PILOTING_CMD_NAVIGATEHOME
-    // ARCOMMANDS_ID_ARDRONE3_PILOTING_CMD_AUTOTAKEOFFMODE
-
+    /// - Captain #Ferris 🦀 :Take off... 🛫
     pub fn take_off(&self) -> AnyResult<()> {
-        // Ardrone3
-        // ARCOMMANDS_ID_ARDRONE3_CLASS_PILOTING
-        // ARCOMMANDS_ID_ARDRONE3_PILOTING_CMD_TAKEOFF
-        // strOffset = ARCOMMANDS_ReadWrite_WriteString ("ARDrone3.Piloting.TakeOff:", resString, stringLen, strOffset) ;
-        // feature: u8 class: u8 cmd_take_off: u16
+        let feature = Feature::ArDrone3(Some(ArDrone3::Piloting(Piloting::TakeOff)));
 
-        let feature = Feature::ArDrone3(ArDrone3::TakeOff);
+        let frame = Frame::for_drone(
+            &self.drone,
+            Type::DataWithAck,
+            BufferID::CDAck,
+            Some(feature),
+        );
+
+        self.drone.send_frame(frame)
+    }
+
+    pub fn landing(&self) -> AnyResult<()> {
+        let feature = Feature::ArDrone3(Some(ArDrone3::Piloting(Piloting::Landing)));
 
         let frame = Frame::for_drone(
             &self.drone,
